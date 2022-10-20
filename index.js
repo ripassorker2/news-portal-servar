c7e4cd0f8fe752367468f8a8a103e6ffa523bbc9
Ripassorker <ripassorker02@glail.com>
2022-10-21 01:07:33 +0600

    added news daata

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const port = process.env.PORT || 5000;
 let cors = require("cors");
 
 const catagories = require("./data/catagories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("news api comming sooo");
+  res.send("news catagory comming soon");
 });
 
 app.get("/news-catagories", (req, res) => {
   res.send(catagories);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/catagory/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const catagoryNews = news.filter((n) => n.category_id === id);
+    res.send(catagoryNews);
+  }
 });
 
 app.listen(port, () => {
